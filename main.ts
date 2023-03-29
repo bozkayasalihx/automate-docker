fee42ffd398fb57984c9baaaae3945f0390b3c20
PF <pf@MacBook-Pro.local>
2023-03-29 17:12:28 +0300

    final version

--- main.ts
+++ main.ts
@@ -1,83 +1,79 @@
-import {execSync, exec, spawn} from 'child_process';
-import path from 'path';
-
+import { execSync, exec, spawn } from "child_process";
+import path from "path";
 
 export class Execute {
-    private dir:string 
-    private Command = "docker run --rm -v `pwd`:/project mingc/android-build-box bash -c 'cd /project; ./gradlew assembleRelease'"
-    private extract = "docker cp android:/project/app/build/outputs/apk/release/app-release-unsigned.apk /home/ubuntu"
+    private dir: string;
+    private Command =
+        "docker run --rm -v `pwd`:/project mingc/android-build-box bash -c 'cd /project; ./gradlew assembleRelease'";
+    private extract =
+        "docker cp android:/project/app/build/outputs/apk/release/app-release-unsigned.apk /home/ubuntu";
 
     constructor(dir: string) {
-        this.dir= dir;
+        this.dir = dir;
     }
-
 
     private check(): boolean {
         let valid: boolean = true;
         exec("docker images", (err, sdtout) => {
             if (err) {
-                console.log("got an error! try again later!\n", err); 
+                console.log("got an error! try again later!\n", err);
                 return err;
-            } 
+            }
             if (sdtout.includes("android")) {
                 valid = true;
             }
-            valid =true
-        })
+            valid = true;
+        });
 
         return valid;
-        
     }
-
 
     private absolutePath(): string {
         return path.resolve(this.dir);
     }
-    
-    private upload() {
 
-    }
-
+    private upload() {}
 
     public build() {
-        if (this.check() == false)  {
+        if (this.check() == false) {
             console.log("please check images was built, and try again later");
             return;
         }
 
-        const path = this.absolutePath(); 
+        const path = this.absolutePath();
         try {
- 
             process.chdir(path);
-            const buff = execSync(this.Command)
-            console.log(buff.toString() + "\n")
-        }catch(err) {
+            const buff = execSync(this.Command);
+            console.log(buff.toString() + "\n");
+        } catch (err) {
             console.log(`couldnt change ${path} with an ${err}`);
         }
 
         try {
             // const buff = execSync(this.keepRunningDocker);
             // console.log(buff.toString() + "\n")
-            const docker = spawn("docker", ["run", "--name", "android", "-v", "`pwd`:/project", "-it", "mingc/android-build-box", "bash"])
-            docker.on("message", (msg) =>  {
+            const docker = spawn("docker", [
+                "run",
+                "--name",
+                "android",
+                "-v",
+                "`pwd`:/project",
+                "-it",
+                "mingc/android-build-box",
+                "bash",
+            ]);
+            docker.on("message", (msg) => {
                 console.log(msg.toString() + "\n");
-            })
-            docker.on("error", code => {
+            });
+            docker.on("error", (code) => {
                 console.log("got an error with this error code", code);
-            })
-            const b = execSync(this.extract)
+            });
+            const b = execSync(this.extract);
             console.log(b.toString());
-        }catch(err) {
+        } catch (err) {
             console.log("couldnt run command", err);
         }
     }
-
-
-
-
-
-
 }
-
 
 export default new Execute("/home/ubuntu/test2");
--- server.ts
+++ server.ts
@@ -1,84 +1,140 @@
-import express from "express"
+import express from "express";
 import execute from "./main";
-import glob from 'glob';
+import glob from "glob";
 import { S3Uploader } from "./uploader";
 import "dotenv/config";
-import fs from 'fs';
+import fs from "fs";
 //@ts-ignore
-import xml2js from  "xml2js";
+import xml2js from "xml2js";
 
-const REGION = process.env.REGION
-const ACCESSKEY = process.env.ACCESSKEY; 
+const REGION = process.env.REGION;
+const ACCESSKEY = process.env.ACCESSKEY;
 const SECRETKEY = process.env.SECRETKEY;
 
+const app = express();
 
-const app = express()
+app.use(express.json());
 
-app.use(express.json())
-
-
-app.get("/bundle/:bundle/:s3address", async(req, res) => {
-    const { bundle, s3address} = req.params
-    if(!bundle || !s3address) return res.sendStatus(400);
-    res.sendStatus(200);
-    // execute.build()
-    // const uploader = new S3Uploader(REGION as string, ACCESSKEY as string, SECRETKEY as string, s3address)
-
-    // let mostrecent: number = 0;
-    // let thefile: string = "";
-    // while (true) {
-    //     const files = await glob("/home/ubuntu/*.apk");
-    //     if (files.length == 0) continue;
-    //     for (let file of files) {
-    //         console.log(file + "\n");
-    //         const lastModified = fs.statSync(file).mtimeMs
-    //         if (lastModified > mostrecent) {
-    //             mostrecent = lastModified;
-    //             thefile = file;
-    //         }
-    //     }
-    //     break;
-    // }
+app.get("/bundle/:bundle/:s3address", async (req, res) => {
+    const { bundle, s3address } = req.params;
+    if (!bundle || !s3address) return res.sendStatus(400);
     try {
-        fs.readFile("/home/ubuntu/test2/app/src/main/AndroidManifest.xml", (err, data) => {
-          if (err) {
-            console.error(err);
-            return;
-          }
+        fs.readFile(
+            "/home/ubuntu/test2/app/src/main/AndroidManifest.xml",
+            (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
 
-          xml2js.parseString(data, (err, result) => {
-            if (err) {
-              console.error(err);
-              return;
+                xml2js.parseString(data, (err, result) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+
+                    console.log(result.manifest.$["android:versionName"]);
+                    result.manifest.$.package = "com.bozkayasalih";
+                    result.manifest.$["android:versionName"] = "1.0.0";
+                    result.manifest.$["android:versionCode"] = "00000";
+                    const builder = new xml2js.Builder();
+                    const updatedXml = builder.buildObject(result);
+
+                    fs.writeFile(
+                        "/home/ubuntu/test2/app/src/main/AndroidManifest.xml",
+                        updatedXml,
+                        (err) => {
+                            if (err) {
+                                console.error(err);
+                                return;
+                            }
+                            console.log("Package field updated successfully!");
+                        }
+                    );
+                });
+                const result = template(bundle);
+                console.log(result);
             }
-
-            // Update the package field
-            console.log(result.manifest.$)
-            result.manifest.$.package = 'com.bozkayasalih';
-
-            // Convert the updated result back to XML
-            const builder = new xml2js.Builder();
-            const updatedXml = builder.buildObject(result);
-
-            // Write the updated XML back to the file
-            fs.writeFile("/home/ubuntu/test2/app/src/main/AndroidManifest.xml", updatedXml, (err) => {
-              if (err) {
-                console.error(err);
-                return;
-              }
-
-              console.log('Package field updated successfully!');
-            });
-          });
-    });
-        
-    }catch(err) {
+        );
+    } catch (err) {
         console.log(`got an error ${err}`);
     }
-})
+    res.sendStatus(200);
+    execute.build();
+    const uploader = new S3Uploader(
+        REGION as string,
+        ACCESSKEY as string,
+        SECRETKEY as string,
+        s3address
+    );
 
+    let mostrecent: number = 0;
+    let thefile: string = "";
+    while (true) {
+        const files = await glob("/home/ubuntu/*.apk");
+        if (files.length == 0) continue;
+        for (let file of files) {
+            console.log(file + "\n");
+            const lastModified = fs.statSync(file).mtimeMs;
+            if (lastModified > mostrecent) {
+                mostrecent = lastModified;
+                thefile = file;
+            }
+        }
+        break;
+    }
+
+    console.log("the file the creator of the universe", thefile);
+});
 
 app.listen(5000, () => {
     console.log("server is running on port ", 5000);
-})
+});
 
+function template(packageBundle: string) {
+    const tmpl = `
+        package cordova.plugin.instantdownload;
+
+        import org.apache.cordova.CordovaPlugin;
+        import org.apache.cordova.CallbackContext;
+
+        import org.json.JSONArray;
+        import org.json.JSONException;
+        import org.json.JSONObject;
+
+        import com.google.android.gms.instantapps.InstantApps;
+        import android.content.Intent;
+        import android.net.Uri;
+
+        public class InstantDownload extends CordovaPlugin {
+
+            @Override
+            public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+                if (action.equals("downloadApp")) {
+                    this.downloadApp( callbackContext );
+                    return true;
+                }
+                return false;
+            }
+
+            private void downloadApp(CallbackContext callbackContext) {
+                Intent postInstallIntent = new Intent(Intent.ACTION_MAIN,
+                                    Uri.parse(${packageBundle})).
+                                    addCategory(Intent.CATEGORY_DEFAULT);
+
+                InstantApps.showInstallPrompt(this.cordova.getActivity(), postInstallIntent, 7, null);
+            }
+        }
+    `;
+    try {
+        fs.writeFileSync(
+            "/home/ubuntu/test2/app/src/main/java/cordova/plugin/instantdownload/InstantDownload/InstantDownload.java",
+            tmpl
+        );
+        return true;
+    } catch (err) {
+        console.log(err);
+        return false;
+    }
+}
