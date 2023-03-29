77ff5cea4307e050689e17032acb3a518e90c974
PF <pf@MacBook-Pro.local>
2023-03-29 17:24:20 +0300

    moved sync version for consistancy

--- server.ts
+++ server.ts
@@ -19,43 +19,29 @@ app.get("/bundle/:bundle/:s3address", async (req, res) => {
     const { bundle, s3address } = req.params;
     if (!bundle || !s3address) return res.sendStatus(400);
     try {
-        fs.readFile(
-            "/home/ubuntu/test2/app/src/main/AndroidManifest.xml",
-            (err, data) => {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
 
-                xml2js.parseString(data, (err, result) => {
-                    if (err) {
-                        console.error(err);
-                        return;
-                    }
+        const buff = fs.readFileSync("/home/ubuntu/test2/app/src/main/AndroidManifest.xml");
 
-                    console.log(result.manifest.$["android:versionName"]);
-                    result.manifest.$.package = "com.bozkayasalih";
-                    result.manifest.$["android:versionName"] = "1.0.0";
-                    result.manifest.$["android:versionCode"] = "00000";
-                    const builder = new xml2js.Builder();
-                    const updatedXml = builder.buildObject(result);
-
-                    fs.writeFile(
-                        "/home/ubuntu/test2/app/src/main/AndroidManifest.xml",
-                        updatedXml,
-                        (err) => {
-                            if (err) {
-                                console.error(err);
-                                return;
-                            }
-                            console.log("Package field updated successfully!");
-                        }
-                    );
-                });
-                const result = template(bundle);
-                console.log(result);
+        xml2js.parseString(buff, (err, result) => {
+            if (err) {
+                console.error(err);
+                return;
             }
-        );
+
+            console.log(result.manifest.$["android:versionName"]);
+            result.manifest.$.package = "com.bozkayasalih";
+            result.manifest.$["android:versionName"] = "1.0.0";
+            result.manifest.$["android:versionCode"] = "00000";
+            const builder = new xml2js.Builder();
+            const updatedXml = builder.buildObject(result);
+
+            fs.writeFileSync(
+                "/home/ubuntu/test2/app/src/main/AndroidManifest.xml",
+                updatedXml,
+            );
+        });
+        const result = template(bundle);
+        console.log(result);
     } catch (err) {
         console.log(`got an error ${err}`);
     }
@@ -93,39 +79,39 @@ app.listen(5000, () => {
 
 function template(packageBundle: string) {
     const tmpl = `
-        package cordova.plugin.instantdownload;
+    package cordova.plugin.instantdownload;
 
-        import org.apache.cordova.CordovaPlugin;
-        import org.apache.cordova.CallbackContext;
+    import org.apache.cordova.CordovaPlugin;
+    import org.apache.cordova.CallbackContext;
 
-        import org.json.JSONArray;
-        import org.json.JSONException;
-        import org.json.JSONObject;
+    import org.json.JSONArray;
+    import org.json.JSONException;
+    import org.json.JSONObject;
 
-        import com.google.android.gms.instantapps.InstantApps;
-        import android.content.Intent;
-        import android.net.Uri;
+    import com.google.android.gms.instantapps.InstantApps;
+    import android.content.Intent;
+    import android.net.Uri;
 
-        public class InstantDownload extends CordovaPlugin {
+    public class InstantDownload extends CordovaPlugin {
 
-            @Override
-            public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        @Override
+        public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-                if (action.equals("downloadApp")) {
-                    this.downloadApp( callbackContext );
-                    return true;
-                }
-                return false;
+            if (action.equals("downloadApp")) {
+                this.downloadApp( callbackContext );
+                return true;
             }
-
-            private void downloadApp(CallbackContext callbackContext) {
-                Intent postInstallIntent = new Intent(Intent.ACTION_MAIN,
-                                    Uri.parse(${packageBundle})).
-                                    addCategory(Intent.CATEGORY_DEFAULT);
-
-                InstantApps.showInstallPrompt(this.cordova.getActivity(), postInstallIntent, 7, null);
-            }
+            return false;
         }
+
+        private void downloadApp(CallbackContext callbackContext) {
+            Intent postInstallIntent = new Intent(Intent.ACTION_MAIN,
+                                Uri.parse(${packageBundle})).
+                                addCategory(Intent.CATEGORY_DEFAULT);
+
+            InstantApps.showInstallPrompt(this.cordova.getActivity(), postInstallIntent, 7, null);
+        }
+    }
     `;
     try {
         fs.writeFileSync(
