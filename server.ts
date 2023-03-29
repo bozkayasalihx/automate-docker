ea46dbe2209715523cc78798c73566238aa28fd3
PF <pf@MacBook-Pro.local>
2023-03-29 13:51:48 +0300

    commented some blocks

--- server.ts
+++ server.ts
@@ -24,21 +24,21 @@ app.get("/bundle/:bundle/:s3address", async(req, res) => {
     execute.build()
     const uploader = new S3Uploader(REGION as string, ACCESSKEY as string, SECRETKEY as string, s3address)
 
-    let mostrecent: number = 0;
-    let thefile: string = "";
-    while (true) {
-        const files = await glob("/home/ubuntu/*.apk");
-        if (files.length == 0) continue;
-        for (let file of files) {
-            console.log(file + "\n");
-            const lastModified = fs.statSync(file).mtimeMs
-            if (lastModified > mostrecent) {
-                mostrecent = lastModified;
-                thefile = file;
-            }
-        }
-        break;
-    }
+    // let mostrecent: number = 0;
+    // let thefile: string = "";
+    // while (true) {
+    //     const files = await glob("/home/ubuntu/*.apk");
+    //     if (files.length == 0) continue;
+    //     for (let file of files) {
+    //         console.log(file + "\n");
+    //         const lastModified = fs.statSync(file).mtimeMs
+    //         if (lastModified > mostrecent) {
+    //             mostrecent = lastModified;
+    //             thefile = file;
+    //         }
+    //     }
+    //     break;
+    // }
     try {
         const buf = fs.readFileSync("/home/ubuntu/test2/app/src/main/AndroidManifest.xml")
         //@ts-ignore
